cef7f361bf3780b27d7a3fd35f678b5576583a8c
Thomas Christinck <thomas.christinck@mail.mcgill.ca>
2016-11-21 12:23:57 -0500

    Still working on fixing bugs with MenuPage

--- ant/Build/test/ca/mcgill/ecse321/FoodTruckManagementSystem/AllTests.java
+++ ant/Build/test/ca/mcgill/ecse321/FoodTruckManagementSystem/AllTests.java
@@ -4,11 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import ca.mcgill.ecse321.FoodTruckManagementSystem.controller.TestEquipmentController;
+import ca.mcgill.ecse321.FoodTruckManagementSystem.controller.TestSupplyController;
 import ca.mcgill.ecse321.FoodTruckManagementSystem.controller.TestStaffController;
 import ca.mcgill.ecse321.FoodTruckManagementSystem.persistence.TestPersistence;
 //Test suite with all tests (testpersistence, test staffcontroller)
 @RunWith(Suite.class)
-@SuiteClasses({TestPersistence.class, TestStaffController.class })
+@SuiteClasses({TestPersistence.class, TestEquipmentController.class, TestStaffController.class, 
+	TestSupplyController.class})
 public class AllTests {
 
 }
